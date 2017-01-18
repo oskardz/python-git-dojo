619fb0eed8a18b945c49e52f6615e818e12d67c3
kborowska20 <canimi18@gmial.com>
2017-01-18 09:40:08 +0100

    person B - section 1 done

--- gittie_helper.py
+++ gittie_helper.py
@@ -25,14 +25,14 @@ class GittieHelper():
         Method sets air pollution level to attribute and validate input
         :param air_pollution_level:
         """
-        pass
+        self.air_pollution = air_pollution_level
 
     def set_day_of_the_year(self, day_number):
         """
         Method sets day number from beginning of the year to attribute and validate input
         :param day_number:
         """
-        pass
+        self.day_of_year = day_number
 
     def get_value(self):
         """
